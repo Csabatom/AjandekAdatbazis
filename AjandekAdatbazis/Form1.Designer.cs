d9f7eaf6d15280bb0fb4b0bb462083fb455f3d8e
Nyirom2 <nyirocsabi@gmail.com>
2020-11-24 13:09:18 +0100

    Add project files.

---
+++ AjandekAdatbazis/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace AjandekAdatbazis
+{
+    partial class AjandekListaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ajandekListBox = new System.Windows.Forms.ListBox();
+            this.TxtBox_AjandekNev = new System.Windows.Forms.TextBox();
+            this.TxtBox_AjandekUzlet = new System.Windows.Forms.TextBox();
+            this.Btn_AjandekHozzaadas = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LBL_AjandekID = new System.Windows.Forms.Label();
+            this.LBL_AjandekNev = new System.Windows.Forms.Label();
+            this.LBL_AjandekUzlet = new System.Windows.Forms.Label();
+            this.Btn_AjandekTorlese = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ajandekListBox
+            // 
+            this.ajandekListBox.FormattingEnabled = true;
+            this.ajandekListBox.ItemHeight = 16;
+            this.ajandekListBox.Location = new System.Drawing.Point(12, 12);
+            this.ajandekListBox.Name = "ajandekListBox";
+            this.ajandekListBox.Size = new System.Drawing.Size(200, 420);
+            this.ajandekListBox.TabIndex = 0;
+            this.ajandekListBox.SelectedIndexChanged += new System.EventHandler(this.ajandekListBox_SelectedIndexChanged);
+            // 
+            // TxtBox_AjandekNev
+            // 
+            this.TxtBox_AjandekNev.ForeColor = System.Drawing.Color.Gray;
+            this.TxtBox_AjandekNev.Location = new System.Drawing.Point(18, 34);
+            this.TxtBox_AjandekNev.Name = "TxtBox_AjandekNev";
+            this.TxtBox_AjandekNev.Size = new System.Drawing.Size(162, 22);
+            this.TxtBox_AjandekNev.TabIndex = 3;
+            this.TxtBox_AjandekNev.Text = "Ajándék neve";
+            this.TxtBox_AjandekNev.Enter += new System.EventHandler(this.TxtBox_AjandekNev_FocusEnter);
+            this.TxtBox_AjandekNev.Leave += new System.EventHandler(this.TxtBox_AjandekNev_FocusLeave);
+            // 
+            // TxtBox_AjandekUzlet
+            // 
+            this.TxtBox_AjandekUzlet.ForeColor = System.Drawing.Color.Gray;
+            this.TxtBox_AjandekUzlet.Location = new System.Drawing.Point(18, 62);
+            this.TxtBox_AjandekUzlet.Name = "TxtBox_AjandekUzlet";
+            this.TxtBox_AjandekUzlet.Size = new System.Drawing.Size(162, 22);
+            this.TxtBox_AjandekUzlet.TabIndex = 4;
+            this.TxtBox_AjandekUzlet.Text = "Üzlet neve";
+            this.TxtBox_AjandekUzlet.Enter += new System.EventHandler(this.TxtBox_AjandekUzlet_FocusEnter);
+            this.TxtBox_AjandekUzlet.Leave += new System.EventHandler(this.TxtBox_AjandekUzlet_FocusLeave);
+            // 
+            // Btn_AjandekHozzaadas
+            // 
+            this.Btn_AjandekHozzaadas.Location = new System.Drawing.Point(18, 95);
+            this.Btn_AjandekHozzaadas.Name = "Btn_AjandekHozzaadas";
+            this.Btn_AjandekHozzaadas.Size = new System.Drawing.Size(162, 36);
+            this.Btn_AjandekHozzaadas.TabIndex = 5;
+            this.Btn_AjandekHozzaadas.Text = "Ajándék hozzáadása";
+            this.Btn_AjandekHozzaadas.UseVisualStyleBackColor = true;
+            this.Btn_AjandekHozzaadas.Click += new System.EventHandler(this.Btn_AjandekHozzaadas_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtBox_AjandekNev);
+            this.groupBox1.Controls.Add(this.Btn_AjandekHozzaadas);
+            this.groupBox1.Controls.Add(this.TxtBox_AjandekUzlet);
+            this.groupBox1.Location = new System.Drawing.Point(218, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 148);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Új ajándék";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Btn_AjandekTorlese);
+            this.groupBox2.Controls.Add(this.LBL_AjandekUzlet);
+            this.groupBox2.Controls.Add(this.LBL_AjandekNev);
+            this.groupBox2.Controls.Add(this.LBL_AjandekID);
+            this.groupBox2.Location = new System.Drawing.Point(218, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(570, 266);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ajándék tulajdonságai";
+            // 
+            // LBL_AjandekID
+            // 
+            this.LBL_AjandekID.AutoSize = true;
+            this.LBL_AjandekID.Location = new System.Drawing.Point(6, 32);
+            this.LBL_AjandekID.Name = "LBL_AjandekID";
+            this.LBL_AjandekID.Size = new System.Drawing.Size(29, 17);
+            this.LBL_AjandekID.TabIndex = 0;
+            this.LBL_AjandekID.Text = "ID: ";
+            // 
+            // LBL_AjandekNev
+            // 
+            this.LBL_AjandekNev.AutoSize = true;
+            this.LBL_AjandekNev.Location = new System.Drawing.Point(6, 49);
+            this.LBL_AjandekNev.Name = "LBL_AjandekNev";
+            this.LBL_AjandekNev.Size = new System.Drawing.Size(41, 17);
+            this.LBL_AjandekNev.TabIndex = 1;
+            this.LBL_AjandekNev.Text = "Név: ";
+            // 
+            // LBL_AjandekUzlet
+            // 
+            this.LBL_AjandekUzlet.AutoSize = true;
+            this.LBL_AjandekUzlet.Location = new System.Drawing.Point(6, 66);
+            this.LBL_AjandekUzlet.Name = "LBL_AjandekUzlet";
+            this.LBL_AjandekUzlet.Size = new System.Drawing.Size(83, 17);
+            this.LBL_AjandekUzlet.TabIndex = 2;
+            this.LBL_AjandekUzlet.Text = "Üzlet neve: ";
+            // 
+            // Btn_AjandekTorlese
+            // 
+            this.Btn_AjandekTorlese.Enabled = false;
+            this.Btn_AjandekTorlese.Location = new System.Drawing.Point(402, 13);
+            this.Btn_AjandekTorlese.Name = "Btn_AjandekTorlese";
+            this.Btn_AjandekTorlese.Size = new System.Drawing.Size(162, 36);
+            this.Btn_AjandekTorlese.TabIndex = 6;
+            this.Btn_AjandekTorlese.Text = "Ajándék törlése";
+            this.Btn_AjandekTorlese.UseVisualStyleBackColor = true;
+            this.Btn_AjandekTorlese.Click += new System.EventHandler(this.Btn_AjandekTorlese_Click);
+            // 
+            // AjandekListaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ajandekListBox);
+            this.Name = "AjandekListaForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ajandekListBox;
+        private System.Windows.Forms.TextBox TxtBox_AjandekNev;
+        private System.Windows.Forms.TextBox TxtBox_AjandekUzlet;
+        private System.Windows.Forms.Button Btn_AjandekHozzaadas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label LBL_AjandekUzlet;
+        private System.Windows.Forms.Label LBL_AjandekNev;
+        private System.Windows.Forms.Label LBL_AjandekID;
+        private System.Windows.Forms.Button Btn_AjandekTorlese;
+    }
+}
+
---
+++ AjandekAdatbazis/Form1.cs
@@ -0,0 +1,251 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AjandekAdatbazis
+{
+    public partial class AjandekListaForm : Form
+    {
+        MySqlConnection conn;
+
+        public AjandekListaForm()
+        {
+            InitializeComponent();
+
+            conn = new MySqlConnection("Server=localhost; Database=ajandek; Uid=root; Pwd=;");
+            conn.Open();
+
+            this.FormClosed += (sender, args) => {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            };
+
+            AdatBetoltes();
+        }
+
+        void AdatBetoltes()
+        {
+            string sql = @"SELECT id, nev, uzlet FROM ajandek ORDER BY nev";
+            var comm = this.conn.CreateCommand();
+            comm.CommandText = sql;
+            using (var reader = comm.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    string nev = reader.GetString("nev");
+
+                    string uzlet;
+                    try
+                    {
+                        uzlet = reader.GetString("uzlet");
+                    }
+                    catch (SqlNullValueException ex)
+                    {
+                        uzlet = null;
+                    }
+                    var ajandek = new Ajandek(id, nev, uzlet);
+                    ajandekListBox.Items.Add(ajandek);
+                }
+            }
+        }
+
+        private void Btn_AjandekHozzaadas_Click(object sender, EventArgs e)
+        {
+            if(Btn_AjandekHozzaadas.Text == "Ajándék hozzáadása")
+            {
+                try
+                {
+                    string ajandekNev = TxtBox_AjandekNev.Text;
+                    string ajandekUzlet;
+                    if (TxtBox_AjandekUzlet.ForeColor == Color.Gray)
+                    {
+                        ajandekUzlet = null;
+                    }
+                    else
+                    {
+                        ajandekUzlet = TxtBox_AjandekUzlet.Text;
+                    }
+                    string sql = "INSERT INTO ajandek.ajandek(nev,uzlet) VALUES('" + ajandekNev + "','" + ajandekUzlet + "');";
+                    MySqlDataReader reader;
+                    reader = new MySqlCommand(sql, conn).ExecuteReader();
+                    while (reader.Read())
+                    {
+                    }
+                    reader.Close();
+                    ajandekListBox.Items.Clear();
+                    AdatBetoltes();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            } else
+            {
+                try
+                {
+                    string ajandekNev = TxtBox_AjandekNev.Text;
+                    string ajandekUzlet;
+                    int ajandekID = ((Ajandek)ajandekListBox.SelectedItem).Id;
+                    if (TxtBox_AjandekUzlet.ForeColor == Color.Gray)
+                    {
+                        ajandekUzlet = null;
+                    }
+                    else
+                    {
+                        ajandekUzlet = TxtBox_AjandekUzlet.Text;
+                    }
+                    string sql = "UPDATE ajandek SET nev='" + ajandekNev + "', uzlet='" + ajandekUzlet + "' WHERE id = " + ajandekID + ";";
+                    MySqlDataReader reader;
+                    reader = new MySqlCommand(sql, conn).ExecuteReader();
+                    while (reader.Read())
+                    {
+                    }
+                    reader.Close();
+                    ajandekListBox.Items.Clear();
+                    AdatBetoltes();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            TxtBox_AjandekNev.Text = "Ajándék neve";
+            TxtBox_AjandekNev.ForeColor = Color.Gray;
+            TxtBox_AjandekUzlet.Text = "Üzlet neve";
+            TxtBox_AjandekUzlet.ForeColor = Color.Gray;
+        }
+
+        private void TxtBox_AjandekNev_FocusEnter(object sender, EventArgs e)
+        {
+            if(TxtBox_AjandekNev.Text == "Ajándék neve" && TxtBox_AjandekNev.ForeColor == Color.Gray)
+            {
+                TxtBox_AjandekNev.Text = "";
+                TxtBox_AjandekNev.ForeColor = Color.Black;
+            }
+
+            if((TxtBox_AjandekNev.Text == "" || TxtBox_AjandekNev.ForeColor == Color.Gray) && (TxtBox_AjandekUzlet.Text == "" || TxtBox_AjandekUzlet.ForeColor == Color.Gray))
+            {
+                Btn_AjandekHozzaadas.Text = "Ajándék hozzáadása";
+            }
+        }
+
+        private void TxtBox_AjandekNev_FocusLeave(object sender, EventArgs e)
+        {
+            if (TxtBox_AjandekNev.Text == "" && TxtBox_AjandekNev.ForeColor == Color.Black)
+            {
+                TxtBox_AjandekNev.Text = "Ajándék neve";
+                TxtBox_AjandekNev.ForeColor = Color.Gray;
+            }
+
+            if ((TxtBox_AjandekNev.Text == "" || TxtBox_AjandekNev.ForeColor == Color.Gray) && (TxtBox_AjandekUzlet.Text == "" || TxtBox_AjandekUzlet.ForeColor == Color.Gray))
+            {
+                Btn_AjandekHozzaadas.Text = "Ajándék hozzáadása";
+            }
+        }
+
+        private void TxtBox_AjandekUzlet_FocusEnter(object sender, EventArgs e)
+        {
+            if ((TxtBox_AjandekUzlet.Text == "Üzlet neve" || TxtBox_AjandekUzlet.Text == "Saját készítésű") && TxtBox_AjandekUzlet.ForeColor == Color.Gray)
+            {
+                TxtBox_AjandekUzlet.Text = "";
+                TxtBox_AjandekUzlet.ForeColor = Color.Black;
+            }
+
+            if ((TxtBox_AjandekNev.Text == "" || TxtBox_AjandekNev.ForeColor == Color.Gray) && (TxtBox_AjandekUzlet.Text == "" || TxtBox_AjandekUzlet.ForeColor == Color.Gray))
+            {
+                Btn_AjandekHozzaadas.Text = "Ajándék hozzáadása";
+            }
+        }
+
+        private void TxtBox_AjandekUzlet_FocusLeave(object sender, EventArgs e)
+        {
+            if (TxtBox_AjandekUzlet.Text == "" && TxtBox_AjandekUzlet.ForeColor == Color.Black)
+            {
+                TxtBox_AjandekUzlet.Text = "Üzlet neve";
+                TxtBox_AjandekUzlet.ForeColor = Color.Gray;
+            }
+
+            if ((TxtBox_AjandekNev.Text == "" || TxtBox_AjandekNev.ForeColor == Color.Gray) && (TxtBox_AjandekUzlet.Text == "" || TxtBox_AjandekUzlet.ForeColor == Color.Gray))
+            {
+                Btn_AjandekHozzaadas.Text = "Ajándék hozzáadása";
+            }
+        }
+
+        private void ajandekListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Ajandek ajandek = (Ajandek)ajandekListBox.SelectedItem;
+            TxtBox_AjandekNev.Text = ajandek.Nev;
+            TxtBox_AjandekNev.ForeColor = Color.Black;
+            if(ajandek.Uzlet == "")
+            {
+                TxtBox_AjandekUzlet.Text = "Saját készítésű";
+                TxtBox_AjandekUzlet.ForeColor = Color.Gray;
+            } else
+            {
+                TxtBox_AjandekUzlet.Text = ajandek.Uzlet;
+                TxtBox_AjandekUzlet.ForeColor = Color.Black;
+            }
+
+            LBL_AjandekID.Text = "ID: ";
+            LBL_AjandekNev.Text = "Név: ";
+            LBL_AjandekUzlet.Text = "Üzlet neve: ";
+            LBL_AjandekID.Text += ajandek.Id.ToString();
+            LBL_AjandekNev.Text += ajandek.Nev;
+            if(ajandek.Uzlet == "")
+            {
+                LBL_AjandekUzlet.Text += "Saját készítésű";
+            } else
+            {
+                LBL_AjandekUzlet.Text += ajandek.Uzlet;
+            }
+            Btn_AjandekHozzaadas.Text = "Ajándék módosítása";
+
+            if(ajandekListBox.SelectedIndex != -1)
+            {
+                Btn_AjandekTorlese.Enabled = true;
+            }
+        }
+
+        private void Btn_AjandekTorlese_Click(object sender, EventArgs e)
+        {
+            Ajandek ajandek = (Ajandek)ajandekListBox.SelectedItem;
+
+            try
+            {
+                string sql = "DELETE FROM ajandek WHERE id = '" + ajandek.Id + "';";
+                MySqlDataReader reader;
+                reader = new MySqlCommand(sql, conn).ExecuteReader();
+                while (reader.Read())
+                {
+                }
+                reader.Close();
+                ajandekListBox.Items.Clear();
+                AdatBetoltes();
+                ajandekListBox.SelectedIndex = -1;
+                TxtBox_AjandekNev.Text = "Ajándék neve";
+                TxtBox_AjandekNev.ForeColor = Color.Gray;
+                TxtBox_AjandekUzlet.Text = "Üzlet neve";
+                TxtBox_AjandekUzlet.ForeColor = Color.Gray;
+
+                LBL_AjandekID.Text = "ID: ";
+                LBL_AjandekNev.Text = "Név: ";
+                LBL_AjandekUzlet.Text = "Üzlet neve: ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
